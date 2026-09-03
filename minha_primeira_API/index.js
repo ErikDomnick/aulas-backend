012a16f8b761b99d83db13b770e64caa2ad1471d
Erik Domnick Favero Maciel dos Santos <erikdomnickmaciel@gmail.com>
2026-09-02 22:20:05 -0300

    a2_02-09-26 listas e objetos

--- minha_primeira_API/index.js
+++ minha_primeira_API/index.js
@@ -2,6 +2,22 @@ import express from 'express';
 
 const app = express(); //primeiro pilar: instancia do express
 
+/*
+ * idLivro -> identificador / int
+ * stTitulo -> string
+ * stAutor -> string
+ * blDisponivel -> boolean
+*/
+
+let meuPrimeiroLivro = {
+    id: 1,
+    stTitulo: "negoney em busca da manga perfeita",
+    stAutor: "Nego Neyson",
+    blDisponivel: true
+};
+
+let livros = [meuPrimeiroLivro];
+
 app.get("/", (req, res) => {
     res.send("seja bem-vindo a gestão de livros");
 });
@@ -12,3 +28,14 @@ app.get("/livros", (pergunta, resposta) => {
 });
 
 app.listen(3000); //terceiro pilar: porta a ser ouvida
+
+
+/*
+cadastrar livros - POST
+buscar todos livros - GET
+buscar um livro pelo nome - GET
+buscar um livro pelo id - GET
+emprestar livro - PATCH
+devolver livro - PATCH
+deletar livro - DELETE
+*/
