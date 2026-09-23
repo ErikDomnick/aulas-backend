a3ba2a77effcb6c19ba0457dd719a504a27226c4
Erik Domnick Favero Maciel dos Santos <erikdomnickmaciel@gmail.com>
2026-09-23 20:24:13 -0300

    criando rota de post

--- minha_primeira_API/index.js
+++ minha_primeira_API/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 
 const app = express(); //primeiro pilar: instancia do express
+app.use(express.json());
 
 /*
  * idLivro -> identificador / int
- * stTitulo -> string
- * stAutor -> string
- * blDisponivel -> boolean
+ * stTitulo -> string = ds
+ * stAutor -> string = ds
+ * blDisponivel -> boolean = fg
 */
 
+let ultimoId = 1;
 let livros = [
     {idLivro: 1, stTitulo: "as conicas de negoney", stAutor: "negoney", blDisponivel: true},
     {idLivro: 2, stTitulo: "as conicas de negoney", stAutor: "negoney", blDisponivel: true}
@@ -22,12 +24,48 @@ app.get("/livros", (req, res) => {
     res.json(livros);
 });
 
-app.get("livros/:id", (req, res) => {
+app.get("/livros/:id", (req, res) => {
     const id = parseInt(req.params.id);
     if(isNaN(id)) {
-        res.status(400).json({mensagem: "o parametro precisa ser um numero valido"});
+        return res
+        .status(400)
+        .json({mensagem: "o parametro precisa ser um numero valido"});
     }
+
+    let livro = livros.find((livro) => {
+        return livro.idLivro === id;
+    });
+
+    if(livro) {
+        res.status(404).send;
+    }
+
+    res.json(livro());
 });
+
+app.post("/livros", (req, res) => {
+    let idNovo = ultimoId++;
+
+    let tituloEnviado = req.body.titulo
+    let autorEnviado = req.body.autor
+
+    if(!tituloEnviado || !autorEnviado){
+        return res
+        .status(400)
+        .json({ mensagem: "dados invalidos, verifique se esta tudo preenchido" });
+    }
+
+    let novoLivro = {
+        idLivro: idNovo,
+        blDispobivel: true,
+        stTitutlo: tituloEnviado,
+        stAutor: autorEnviado
+    };
+
+    livros.push(novoLivro);
+
+    res.status(201).json(livros);
+})
 
 app.listen(3000); //terceiro pilar: porta a ser ouvida
 
